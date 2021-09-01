1f6ec11e41fc4c927e1f3f990ef31afa67b25b37
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-01 16:45:38 +0300

    add hw1

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
-
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
 const refs = {
   startBtn: document.querySelector('[data-action="start"]'),
-  stopBtn: document.querySelector('[data-action="stop"]')
+  stopBtn: document.querySelector('[data-action="stop"]'),
 };
 
-refs.startBtn.addEventListener("click", onStartBtnClick);
-refs.stopBtn.addEventListener("click", onStopBtnClick);
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
 
 let id;
 
@@ -16,14 +22,14 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 function randomNum() {
-  const random = randomIntegerFromInterval(0, colors.length - 1)
+  const random = randomIntegerFromInterval(0, colors.length - 1);
   document.body.style.backgroundColor = colors[random];
 }
 
 function onStartBtnClick() {
   id = setInterval(randomNum, 1000);
   refs.startBtn.disabled = true;
-};
+}
 
 function onStopBtnClick() {
   refs.startBtn.disabled = false;
